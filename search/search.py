62c4312cc12f492e53ec532f098b2610c6c8e887
Franzisa Rothen <franziska.rothen@hotmail.com>
2021-10-23 16:01:18 +0200

    so far so good

--- search/search.py
+++ search/search.py
@@ -19,6 +19,8 @@ Pacman agents (in searchAgents.py).
 
 import util
 
+visited_nodes = []
+
 class SearchProblem:
     """
     This class outlines the structure of a search problem, but doesn't implement
@@ -80,15 +82,18 @@ def tanslateToDirections(path):
     return final
 
 
-def discoverPath(problem, current_node, paths):
+def discoverPath(problem, current_node, path):
     i = True
     while i:
+        if problem.isGoalState(current_node):
+            is_goal = True
+            break
         # pick first successor and set as the next node
         next_node = problem.getSuccessors(current_node)[0][0]
         # find number of successors
         number_of_successors = len(problem.getSuccessors(current_node))
         # check all the successors if they have been discovered
-        if next_node in paths:
+        if next_node in visited_nodes:
             if number_of_successors == 0:
                 next_node = problem.getSuccessors(current_node)[1][0]
                 i = False
@@ -96,17 +101,20 @@ def discoverPath(problem, current_node, paths):
             else:
                 for x in range(number_of_successors-1):
                     next_node = problem.getSuccessors(current_node)[x+1][0]
-                    if next_node in paths:
+                    if next_node in visited_nodes:
                         i = False
                         is_goal = False
 
         # if next node isn't already explored, append it to paths
-        paths.append(next_node)
+        path.append(next_node)
+        if next_node not in visited_nodes:
+            visited_nodes.append(next_node)
         current_node = next_node
         if problem.isGoalState(current_node):
             is_goal = True
+            break
 
-    return is_goal, paths
+    return is_goal, path
 
 
 def tinyMazeSearch(problem):
@@ -133,23 +141,48 @@ def depthFirstSearch(problem):
     print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
+    global visited_nodes
 
     # find coordinates of initial state
     current_node = problem.getStartState()
 
     # create vector which will be filled with found path, initialize with first position coordinates
-    paths = []
-    paths.append(current_node)
+    path = []
+    path.append(current_node)
+    visited_nodes.append(current_node)
 
-    is_goal, paths = discoverPath(problem, current_node, paths)
+    # call function to find a first path
+    is_goal, path = discoverPath(problem, current_node, path)
 
-    print(is_goal, paths)
-
+    # if we found the goal, return the path
     if is_goal:
-        winningPath = tanslateToDirections(paths)
-        print(winningPath)
+        winning_path = tanslateToDirections(path)
+        print("winner", winning_path)
+    # if we didn't find the goal, repeat with new direction after last good node
     else:
-        pass
+        for i in range(len(path)):
+            number_of_successors = len(problem.getSuccessors(path[-1-i]))
+            if number_of_successors > 2:
+                last_good_node = path[-1-i]
+                break
+        index = path.index(last_good_node)
+        path = path[:index+1]
+        for i in range(number_of_successors):
+            if problem.getSuccessors(last_good_node)[i][0] not in visited_nodes:
+                new_next_node = problem.getSuccessors(last_good_node)[i][0]
+                path.append(new_next_node)
+                visited_nodes.append(new_next_node)
+                break
+
+        is_goal, path = discoverPath(problem, new_next_node, path)
+
+    print(path)
+    if is_goal:
+        winning_path = tanslateToDirections(path)
+        print("winner", winning_path)
+
+    print(visited_nodes)
+
 
     from game import Directions
     n = Directions.NORTH
